16d4a64110fb6cc98e6ee7e985d4b7625901b465
John Stroud <john.stroud@topgolf.com>
2017-04-11 18:04:39 -0500

    no message

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,11 +1,19 @@
 node 'puppetagent-vagrant.topgolfusa.com' {
+
   file { '/info.txt':
     ensure  => 'present',
     content => inline_template("Created by Puppet at <%= Time.now %>\n"),
   }
+
   package { 'ntp':
     ensure => 'installed',
   }
+
+  service { 'ntp':
+    ensure => running,
+    enable => true,
+  }
+
 }
 
 # node 'wikitest' {
