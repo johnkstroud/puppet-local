05e8397524d93c7200d40bb81ecf6866fb443bf4
John Stroud <john.stroud@topgolf.com>
2017-04-11 15:43:00 -0500

    fix node name

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,4 @@
-node 'puppetagent-vagrant' {
+node 'puppetagent-vagrant.topgolfusa.com' {
   file { '/info.txt':
     ensure  => 'present',
     content => inline_template("Created by Puppet at <%= Time.now %>\n")
