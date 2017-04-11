3c7f45564b6f02dd59d854c2dd96190a3f8ab708
John Stroud <john.stroud@topgolf.com>
2017-04-11 17:59:27 -0500

    ntp package

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,10 @@
 node 'puppetagent-vagrant.topgolfusa.com' {
   file { '/info.txt':
     ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n")
+    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
+  }
+  package { 'ntp':
+    ensure => 'installed',
   }
 }
 
