9a46a904768865167ddb70b76042e361a41da70a
John Stroud <john.stroud@topgolf.com>
2017-04-12 17:23:08 -0500

    msql fix

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -54,18 +54,18 @@ class mediawiki {
   }
 
   class { '::apache':
-    docroot     => '/var/www/html',
-    mpm_module  => 'prefork',
-    subscribe   => Package[$phpmysql],
+    docroot    => '/var/www/html',
+    mpm_module => 'prefork',
+    subscribe  => Package[$phpmysql],
   }
 
   class { '::apache::mod::php':}
 
   vcsrepo { '/var/www/html':
-    ensure    => 'present',
-    provider  => 'git',
-    source    => "https://github.com/wikimedia/mediawiki.git",
-    revision  => 'REL_23',
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/wikimedia/mediawiki.git',
+    revision => 'REL_23',
   }
 
   file { '/var/www/html/index.html':
